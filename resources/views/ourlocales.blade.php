f46f64b30a1e45b431ec9f919cadb594c9533062
Oscar Portillo <Oscaritoportilllo@outlook.com>
2024-07-29 15:33:01 -0600

    cambios y configuracion para vistas, tambien configuracion de sistema de login y registro

---
+++ resources/views/ourlocales.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    <p>Nuestros locales</p>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('aboutus', function(){
+    return  view('aboutus');
+})->name('aboutus');
 
+Route::get('ourlocales', function(){
+    return view('ourlocales');
+})->name('ourlocales');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+
+});
