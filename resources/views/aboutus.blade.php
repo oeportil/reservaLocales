a3700a75f6c2da2c252b248531ca1622f69938c2
xaldakr <50474978+xaldakr@users.noreply.github.com>
2024-07-29 21:15:23 -0600

    nada de cod

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,3 +1,7 @@
 <x-app-layout>
-    
+    <div class="text-center flex flex-col justify-center items-center">
+        <div class="z-10">
+            <h1 class="text-black my-7" style="font-size: 4rem; font-weight: bold; text-transform: uppercase;">FITO MOLINA LOCALES</h1>
+        </div>
+    </div>
 </x-app-layout>
