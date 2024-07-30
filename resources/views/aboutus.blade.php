7d75d1aece5ec8c2ca217575fa1625cf3aa63656
Oscar Portillo <Oscaritoportilllo@outlook.com>
2024-07-29 21:40:57 -0600

    Es Hora del commit

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,7 +1,51 @@
 <x-app-layout>
-    <div class="text-center flex flex-col justify-center items-center">
+    <div class="text-center flex flex-col justify-center items-center contenedor ">
         <div class="z-10">
-            <h1 class="text-black my-7" style="font-size: 4rem; font-weight: bold; text-transform: uppercase;">FITO MOLINA LOCALES</h1>
+            <h1 class="text-black my-7" style="font-size: 2.5rem; font-weight: bold; text-transform: uppercase;">FITO MOLINA LOCALES</h1>
         </div>
+       <div class="flex flex-col items-center justify-center">
+            <div class="flex gap-4 justify-between items-center">
+                <p class="text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut mauris nisl. 
+                Aliquam bibendum ac nulla ut euismod. Donec blandit semper gravida. 
+                Sed mollis sem arcu, vulputate tincidunt est semper at. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+                Pellentesque rutrum pretium augue sit amet cursus. Sed pretium libero et aliquam aliquet. Phasellus sit amet luctus leo, 
+                sed dapibus orci. In bibendum elit et urna faucibus, id cursus mauris interdum. Phasellus tincidunt libero at dui molestie aliquet.
+                </p>
+                <img style="max-width: 500px; width: 90%;" src="https://media-public.canva.com/pANQk/MADyRWpANQk/1/s2.jpg" alt="">           
+            </div>
+            <div class="z-10">
+                <h1 class="text-black my-7" style="font-size: 2.5rem; font-weight: bold; text-transform: uppercase;">Nuestra historia</h1>
+            </div>
+            <div class="flex gap-4 justify-between items-center">
+                <img style="max-width: 500px; width: 90%;" src="https://media-public.canva.com/OlY_U/MADyRtOlY_U/1/s2.jpg" alt="">    
+                <p class="text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut mauris nisl. 
+                Aliquam bibendum ac nulla ut euismod. Donec blandit semper gravida. 
+                Sed mollis sem arcu, vulputate tincidunt est semper at. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+                Pellentesque rutrum pretium augue sit amet cursus. Sed pretium libero et aliquam aliquet. Phasellus sit amet luctus leo, 
+                sed dapibus orci. In bibendum elit et urna faucibus, id cursus mauris interdum. Phasellus tincidunt libero at dui molestie aliquet.
+                </p>                       
+            </div>
+       </div>
+       <div class="contenedor py-8">
+            <div class="md:flex">
+                <div class="bg-principal p-4 rounded-s-xl shadow-2xl text-white">
+                    <h3 class="text-3xl mb-4">Mision</h3>
+                    <p>Maecenas ornare lacus turpis. Integer eu neque sit amet felis venenatis ullamcorper. 
+                        Nunc est dui, rhoncus rutrum dictum a, volutpat quis lacus. 
+                        Integer quis urna egestas, tristique nulla vitae, viverra nisi. 
+                        Etiam dolor lectus, sodales convallis ultrices eget, ultricies vitae felis.</p>
+                </div>
+                <div class="bg-white p-4 rounded-e-xl shadow-2xl ">
+                    <h3 class="text-3xl mb-4">Visión</h3>
+                    <p>Maecenas ornare lacus turpis. 
+                        Integer eu neque sit amet felis venenatis ullamcorper. 
+                        Nunc est dui, rhoncus rutrum dictum a, volutpat quis lacus. Integer quis urna egestas, tristique nulla vitae, viverra nisi.
+                        Etiam dolor lectus, sodales convallis ultrices eget, ultricies vitae felis.
+                        JOptionPane.ShowInputDialog</p>
+                </div>
+            </div>
+       </div>
     </div>
 </x-app-layout>
