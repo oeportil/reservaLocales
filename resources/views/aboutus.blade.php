1fada6a4dc1cccf8c8ebb557e8e7b19ebb6203a9
Oscar Portillo <Oscaritoportilllo@outlook.com>
2024-07-29 21:59:39 -0600

    Es hora del Commit

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -4,7 +4,7 @@
             <h1 class="text-black my-7" style="font-size: 2.5rem; font-weight: bold; text-transform: uppercase;">FITO MOLINA LOCALES</h1>
         </div>
        <div class="flex flex-col items-center justify-center">
-            <div class="flex gap-4 justify-between items-center">
+            <div class="md:flex gap-4 justify-between items-center">
                 <p class="text-justify">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut mauris nisl. 
                 Aliquam bibendum ac nulla ut euismod. Donec blandit semper gravida. 
@@ -17,7 +17,7 @@
             <div class="z-10">
                 <h1 class="text-black my-7" style="font-size: 2.5rem; font-weight: bold; text-transform: uppercase;">Nuestra historia</h1>
             </div>
-            <div class="flex gap-4 justify-between items-center">
+            <div class="md:flex gap-4 justify-between items-center">
                 <img style="max-width: 500px; width: 90%;" src="https://media-public.canva.com/OlY_U/MADyRtOlY_U/1/s2.jpg" alt="">    
                 <p class="text-justify">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut mauris nisl. 
@@ -30,14 +30,14 @@
        </div>
        <div class="contenedor py-8">
             <div class="md:flex">
-                <div class="bg-principal p-4 rounded-s-xl shadow-2xl text-white">
+                <div class="bg-principal p-4 md:rounded-s-xl shadow-2xl text-white">
                     <h3 class="text-3xl mb-4">Mision</h3>
                     <p>Maecenas ornare lacus turpis. Integer eu neque sit amet felis venenatis ullamcorper. 
                         Nunc est dui, rhoncus rutrum dictum a, volutpat quis lacus. 
                         Integer quis urna egestas, tristique nulla vitae, viverra nisi. 
                         Etiam dolor lectus, sodales convallis ultrices eget, ultricies vitae felis.</p>
                 </div>
-                <div class="bg-white p-4 rounded-e-xl shadow-2xl ">
+                <div class="bg-white p-4 md:rounded-e-xl shadow-2xl ">
                     <h3 class="text-3xl mb-4">Visión</h3>
                     <p>Maecenas ornare lacus turpis. 
                         Integer eu neque sit amet felis venenatis ullamcorper. 
